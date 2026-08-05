fbe69fec832b7499f66d6ad33cd40399260ff8ed
Alok G S <alokgsgowda@gmail.com>
2026-08-05 21:35:37 +0530

    Aug 5th 2026

---
+++ arrays/LC1752.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int N=nums.size();
+        int count=1;
+
+        if(N==1){
+            return true;
+        }
+        for(int i=1; i<2*N; i++){
+            if(nums[(i-1)%N]<=nums[i%N]){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            if(count==N){
+                return true;
+            }
+            
+        }
+        return false;
+        
+    }
+};
