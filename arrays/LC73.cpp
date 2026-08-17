23d6847de7b898bd725f37f86b39eef57a110378
Alok G S <alokgsgowda@gmail.com>
2026-08-18 00:25:07 +0530

    added optimal solution for lc73

--- arrays/LC73.cpp
+++ arrays/LC73.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
+        int n=matrix.size();       // matrix[..][0]
+        int m=matrix[0].size();    //  matrix[0][...]
+        int col0=1;
+        
 
-        vector<int> row(rows, 0);
-        vector<int> col(cols, 0);
-
-        // Find all rows and columns that contain 0
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
                 }
             }
         }
-
-        // Set corresponding rows and columns to 0
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
+        if(matrix[0][0]==0){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+        
     }
 };
